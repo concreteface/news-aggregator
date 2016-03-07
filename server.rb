d81354d2e4fc61b6e8aa2eca0fdaf46fdb3330fa
concreteface <concreteface@users.noreply.github.com>
2016-03-07 12:28:32 -0500

    initial commit

---
+++ server.rb
@@ -0,0 +1,22 @@
+require "sinatra"
+require "pg"
+require_relative "./app/models/article"
+
+set :views, File.join(File.dirname(__FILE__), "app", "views")
+
+configure :development do
+  set :db_config, { dbname: "news_aggregator_development" }
+end
+
+configure :test do
+  set :db_config, { dbname: "news_aggregator_test" }
+end
+
+def db_connection
+  begin
+    connection = PG.connect(Sinatra::Application.db_config)
+    yield(connection)
+  ensure
+    connection.close
+  end
+end
