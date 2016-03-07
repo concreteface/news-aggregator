6f5f0199b344816fa181aa60b028b82d56f45260
concreteface <concreteface@users.noreply.github.com>
2016-03-07 15:19:15 -0500

    about to monkey with valid again..have 26 successes

--- app/models/article.rb
+++ app/models/article.rb
@@ -14,7 +14,7 @@ class Article
 
   def db_connection
     begin
-      connection = PG.connect(dbname: "news_aggregator_development")
+      connection = PG.connect(dbname: "news_aggregator_test")
       yield(connection)
       ensure
       connection.close
@@ -36,34 +36,44 @@ class Article
     result.each do |res|
       @url_list << res['url']
     end
+    @url_list.include? @url
   end
 
   def url_validator
-      @url =~ URI::regexp
+    @url =~ URI::regexp
   end
 
   def valid?
-    existence_checker
+    # existence_checker
     if @title == '' || @url == '' || @description == ''
-    @errors <<  "Please completely fill out form"
-    false
-  elsif url_validator != 0
-      @errors << "Invalid URL"
-      false
-    elsif @url_list.include? @url
-      @errors << "Article with same url already submitted"
-      false
-    elsif @description.length < 20
-      @errors << "Description must be at least 20 characters long"
-      false
-    else true
+      @errors <<  "Please completely fill out form"
+      return false
     end
+    if url_validator != 0
+      @errors << "Invalid URL"
+      return false
+    end
+    if existence_checker
+      @errors << "Article with same url already submitted"
+      return false
+    end
+    if @description.length < 20
+      @errors << "Description must be at least 20 characters long"
+      return false
+    end
+    return true
   end
 end
 
-# url = 'https://www.google.com'
-# url2 = 'https://www.twitter.com'
-# puts "yo".is_a?(URI::HTTP)
-# puts url.is_a?(URI::HTTP)
-# puts url =~ URI::regexp
-# puts  url2 =~ URI::regexp
+# def db_connection
+#   begin
+#     connection = PG.connect(dbname: "news_aggregator_development")
+#     yield(connection)
+#     ensure
+#     connection.close
+#   end
+# end
+# result = db_connection {|conn| conn.exec("SELECT * FROM articles")}
+# result.each do |res|
+#   puts res['url']
+# end
