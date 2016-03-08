e342726dfcd50d5da6978f701590201ad699cd9e
concreteface <concreteface@users.noreply.github.com>
2016-03-08 08:09:45 -0500

    about to change article display

--- server.rb
+++ server.rb
@@ -27,41 +27,24 @@ get '/' do
 end
 
 get '/articles' do
-
   @articles = db_connection {|conn| conn.exec("SELECT * FROM articles")}
   erb :articles
 end
 
 get '/articles/new' do
+  @errors = []
   erb :submit_article
 end
 
 post '/articles/new' do
-  url_list = []
-  result = db_connection {|conn| conn.exec("SELECT * FROM articles")}
-  result.each do |res|
-    url_list << res['url']
-  end
-  url = URI.parse(params[:url].split.join)
-  if params[:url].empty? || params[:title].empty? || params[:description].empty?
-    @missing_field = true
-  end
-  if params[:description].length < 20
-    @short_description = true
-  end
-  if url.is_a?(URI::HTTP) == false
-    @invalid_url = true
-  end
-  if url_list.include? params[:url]
-    @repeat = true
-  end
-  @url = params[:url]
+  article = Article.new({"title" => params[:title], "url" => params[:url], "description" => params[:description]})
   @title = params[:title]
+  @url = params[:url]
   @description = params[:description]
-  if !@repeat && !@invalid_url && !@short_description && !@missing_url
-    db_connection {|conn| conn.exec_params("INSERT INTO articles (title, url, description) VALUES ($1, $2, $3)", [params[:title], params[:url], params[:description]])}
+  if article.valid?
+    article.save
     redirect '/articles'
-  else erb :submit_article
+  else @errors = article.errors
+    erb :submit_article
   end
-
 end
